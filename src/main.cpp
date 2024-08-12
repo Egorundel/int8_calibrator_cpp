d946b55b125308a886c1f4581b1643493d0b7a9f
egorundel <egorundel@internet.ru>
2024-08-12 14:34:26 +0300

    edited code #9

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,5 @@
 #include "int8_entropy_calibrator.h"
 
-#include <map>
-#include <vector>
-#include <string>
-
 int main() {
 
     Logger logger;
@@ -11,39 +7,67 @@ int main() {
     const char* calibrationImagesDir = "../data/";
     const char* cacheFile = "./calibration_data.cache";
 
-    // Create TensorRT builder and network
+    /* 
+        create TensorRT builder and network
+    */
     nvinfer1::IBuilder *builder = nvinfer1::createInferBuilder(logger);
     initLibNvInferPlugins(&logger, "");
 
 
-    // For expicit batch:
+    /*
+        for expicit batch:
+    */
     uint32_t flag = 1U <<static_cast<uint32_t>(nvinfer1::NetworkDefinitionCreationFlag::kEXPLICIT_BATCH);
     nvinfer1::INetworkDefinition *network = builder->createNetworkV2(flag);
     
-    // For implicit batch:
+    /*
+        for implicit batch:
+    */
     // nvinfer1::INetworkDefinition *network = builder->createNetworkV2(0U);
 
 
-    // Parse ONNX model
+    /*
+        parse ONNX model
+    */
     nvonnxparser::IParser *parser = nvonnxparser::createParser(*network, logger);
     parser->parseFromFile("../onnx_model/yolov8m.onnx", static_cast<int32_t>(nvinfer1::ILogger::Severity::kWARNING));
 
     std::vector<int> sizeList = getTensorSizes(network);
     
-    // Create optimization profile
+    /* 
+        create optimization profile
+    */
     nvinfer1::IOptimizationProfile* profile = builder->createOptimizationProfile();
     profile->setDimensions(network->getInput(0)->getName(), nvinfer1::OptProfileSelector::kMIN, nvinfer1::Dims4{1, 3, network->getInput(0)->getDimensions().d[2], network->getInput(0)->getDimensions().d[3]});
     profile->setDimensions(network->getInput(0)->getName(), nvinfer1::OptProfileSelector::kOPT, nvinfer1::Dims4{6, 3, network->getInput(0)->getDimensions().d[2], network->getInput(0)->getDimensions().d[3]});
     profile->setDimensions(network->getInput(0)->getName(), nvinfer1::OptProfileSelector::kMAX, nvinfer1::Dims4{12, 3, network->getInput(0)->getDimensions().d[2], network->getInput(0)->getDimensions().d[3]});
 
-    // Create builder config
+    /*
+        create builder config
+    */
     nvinfer1::IBuilderConfig *config = builder->createBuilderConfig();
     config->addOptimizationProfile(profile);
-    size_t workspace_size = (1ULL << 30);
-    config->setMemoryPoolLimit(nvinfer1::MemoryPoolType::kWORKSPACE, workspace_size);
+
+    /* 
+        Memory allocation
+    */
+    // size_t workspace_size = (1ULL << 30);
+    // config->setMemoryPoolLimit(nvinfer1::MemoryPoolType::kWORKSPACE, workspace_size);
     // config->setMemoryPoolLimit(nvinfer1::MemoryPoolType::kWORKSPACE, 1U << 20);
 
-    // Set INT8 mode and calibrator
+    /* 
+        comment this out if you don't need detailed information about the engine layers.
+    */
+    // config->setProfilingVerbosity(nvinfer1::ProfilingVerbosity::kDETAILED);
+
+    /* 
+        comment this out if you want to create an engine with ONLY int8 precision.
+    */
+    // config->setFlag(nvinfer1::BuilderFlag::kFP16);
+
+    /*
+        Set INT8 mode and calibrator
+    */
     config->setFlag(nvinfer1::BuilderFlag::kINT8);
     Int8EntropyCalibrator calibrator(
         6, 
@@ -57,8 +81,10 @@ int main() {
     config->setInt8Calibrator(&calibrator);
 
 
-    // Build engine
-    nvinfer1::IHostMemory *engine = builder->buildSerializedNetwork(*network, *config);
+    /* 
+        Build engine
+    */
+    nvinfer1::IHostMemory *plan = builder->buildSerializedNetwork(*network, *config);
 
     std::ofstream engine_file("./yolov8m.engine", std::ios::binary);
     if (!engine_file)
@@ -66,13 +92,15 @@ int main() {
         std::cout << "write serialized file failed\n";
     }
     assert(engine_file.is_open() && "Failed to open engine file");
-    engine_file.write((char *)engine->data(), engine->size());
+    engine_file.write((char *)plan->data(), plan->size());
     engine_file.close();
 
     std::cout << "Engine build success!" << std::endl;
 
-    // Clean up
-    delete engine;
+    /* 
+        clean up
+    */
+    delete plan;
     delete config;
     delete parser;
     delete network;
